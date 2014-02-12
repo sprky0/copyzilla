04c5e9355d5418d1dfd32c629fe1aeb6ff961f9c
Avery Brooks <averyjamesbrooks@gmail.com>
2014-02-12 11:25:29 -0500

    new build script

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-tar -czf ../dist/copyzilla.tar.gz ../src/Copyzilla.app
-zip -r9 ../dist/copyzilla.zip ../src/Copyzilla.app
+cd ../src
+tar -czvf ../dist/copyzilla.tgz Copyzilla.app
+zip -r9 ../dist/copyzilla.zip Copyzilla.app
